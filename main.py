74a19c2f2a0a37a9bcce8f455c2a5d0d755d0cd0
zhongqi <zhongqi90@gmail.com>
2012-05-04 11:03:19 +0800

    finish the hw2 of unit2

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-#
 
 import cgi
 import os
+import re
 
 from google.appengine.ext.webapp import template
 import webapp2
@@ -42,7 +42,58 @@ class Rot13Handler(webapp2.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'rot13.html')
         self.response.out.write(template.render(path, paras))
 
+        
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASSWD_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+class SignupHandler(webapp2.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'signup.html')
+        self.response.out.write(template.render(path, {}))
+        
+    def post(self):
+        path = os.path.join(os.path.dirname(__file__), 'signup.html')
+        
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+        
+        error_username = ''
+        error_password = ''
+        error_email = ''
+        error_verify = ''
+        if not USER_RE.match(username):
+            error_username = "That's not a valid username."
+        if not PASSWD_RE.match(password):
+            error_password = "That wasn't a valid password."
+        elif password != verify:
+            error_verify = "Your passwords didn't match."
+        if ( email ) and ( not EMAIL_RE.match(email) ):
+            error_email = "That's not a valid email."
+        
+        error_and_raw = {'error_username' : error_username,
+                    'error_password' : error_password,
+                    'error_email' : error_email,
+                    'error_verify' : error_verify,
+                    'raw_username' : username,
+                    'raw_email' : email}
+        
+        if error_username or error_password or error_email or error_verify:
+            self.response.out.write(template.render(path, error_and_raw))
+        else:
+            self.redirect('/welcome/?username=%s' % username)
+
+        
+class WelcomeHandler(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get("username")
+        self.response.out.write("Welcome, %s" % username)
+    
+    
 app = webapp2.WSGIApplication([
             ('/', MainHandler),
-            ('/rot13/', Rot13Handler)
+            ('/rot13/', Rot13Handler),
+            ('/signup/', SignupHandler),
+            ('/welcome/', WelcomeHandler)
         ], debug=True)
